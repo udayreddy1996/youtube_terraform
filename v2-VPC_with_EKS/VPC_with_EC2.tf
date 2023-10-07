7728ccfb28926d8ff6d5069ba4f831a90a0e4bdf
udayreddy1996 <90882118+udayreddy1996@users.noreply.github.com>
2023-10-07 22:27:14 +0530

    Update VPC_with_EC2.tf

--- v2-VPC_with_EKS/VPC_with_EC2.tf
+++ v2-VPC_with_EKS/VPC_with_EC2.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "demo-server" {
  ami = var.os_name
- key_name = var.key 
+ key_name = var.key
  instance_type  = var.instance-type
  associate_public_ip_address = true
-subnet_id = aws_subnet.demo_subnet.id
+subnet_id = aws_subnet.demo_subnet-1.id
 vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
 }
 
@@ -17,18 +17,26 @@ resource "aws_vpc" "demo-vpc" {
 }
 
 // Create Subnet
-resource "aws_subnet" "demo_subnet" {
-  vpc_id     = aws_vpc.demo-vpc.id 
+resource "aws_subnet" "demo_subnet-1" {
+  vpc_id     = aws_vpc.demo-vpc.id
   cidr_block = var.subnet1-cidr
   availability_zone = var.subent_az
 
   tags = {
-    Name = "demo_subnet"
+    Name = "demo_subnet-1"
   }
 }
+// Create Subnet
+resource "aws_subnet" "demo_subnet-2" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = var.subnet2-cidr
+  availability_zone = var.subent_az
 
+  tags = {
+    Name = "demo_subnet-2"
+  }
+}
 // Create Internet Gateway
-
 resource "aws_internet_gateway" "demo-igw" {
   vpc_id = aws_vpc.demo-vpc.id
 
@@ -49,17 +57,23 @@ resource "aws_route_table" "demo-rt" {
   }
 }
 
-// associate subnet with route table 
-resource "aws_route_table_association" "demo-rt_association" {
-  subnet_id      = aws_subnet.demo_subnet.id 
+// associate subnet with route table
+resource "aws_route_table_association" "demo-rt_association-1" {
+  subnet_id      = aws_subnet.demo_subnet-1.id
 
   route_table_id = aws_route_table.demo-rt.id
 }
-// create a security group 
+// associate subnet with route table
+resource "aws_route_table_association" "demo-rt_association-2" {
+  subnet_id      = aws_subnet.demo_subnet-2.id
+
+  route_table_id = aws_route_table.demo-rt.id
+}
+// create a security group
 
 resource "aws_security_group" "demo-vpc-sg" {
   name        = "demo-vpc-sg"
- 
+
   vpc_id      = aws_vpc.demo-vpc.id
 
   ingress {
@@ -83,5 +97,13 @@ resource "aws_security_group" "demo-vpc-sg" {
     Name = "allow_tls"
   }
 }
-
-
+module "sgs" {
+    source = "./sg_eks"
+    vpc_id = aws_vpc.demo-vpc.id 
+}
+module "eks" {
+    source = "./eks"
+    sg_ids = module.sgs.security_group_public
+    vpc_id = aws_vpc.demo-vpc.id
+    subnet_ids = [aws_subnet.demo_subnet-1.id,aws_subnet.demo_subnet-2.id]
+}
